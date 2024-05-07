b6fe13923ffb36cdd963b2d4e54a7b9cecfc4d3a
Fuza <tronglai301@gmail.com>
2024-05-07 13:42:29 +0700

    Update structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import ButtonAppBar from './components/navbar/navbar'
-import ButtonAppBarTest from './components/navbar/navbar'
-import Footer from './components/footer/footer';
-import Header from './components/header/header'
-import { Box, Toolbar, Typography } from '@mui/material';
+import { Routes } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Header />
-      <ButtonAppBarTest/>
-      <Footer/>
+      <Routes>
+        
+      </Routes>
     </>
 
   );
