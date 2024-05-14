b168fbe00077f811c210676efc80ad038a4ca958
taminhtri20 <taminhtri20@gmail.com>
2024-05-14 15:08:57 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <>
             <Routes>
                 <Route path={"/"} element={
-                    <AuthContext><Users /></AuthContext>
+                    <AuthContext><HomePage /></AuthContext>
                 } />
                 <Route path={"/users"} element={
                     <AuthContext><Users /></AuthContext>
--- src/components/pages/login/login.js
+++ src/components/pages/login/login.js
@@ -66,7 +66,7 @@ export default function Login() {
                 if (res.data.code === "200") {
                     localStorage.setItem("currentUser", JSON.stringify(res.data.data))
                     enqueueSnackbar('Đăng nhập thành công !', { variant: "success", anchorOrigin: { horizontal: "right", vertical: "top" } });
-                    navigate("/")
+                    navigate("/users")
                 }
                 setFlagValidate({ ...flagValidate, validSubmit: true })
             }
