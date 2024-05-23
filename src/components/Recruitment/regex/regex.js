bd02da4537095ffed7d394aefc4309dedd06ee7b
Nguyenhoang164 <kamito062004@gmail.com>
2024-05-23 13:27:59 +0700

    fix all and done

--- src/components/Recruitment/regex/regex.js
+++ src/components/Recruitment/regex/regex.js
@@ -1,4 +1,5 @@
 
 export const validPhone = new RegExp('^(0|\\+84)[1-9]\\d{8}$');
 export const validEmail = new RegExp('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$');
+export const validFullName = new RegExp('^[\\p{L}][\\p{L}\\s\\-]*$', 'u');
 
