1097655f3a04bbcd964b1fd4852c0c89a6ab4a99
elpsycongry <vuthanhtungcv1@gmail.com>
2024-05-29 03:26:28 +0700

    done

--- src/components/Notification/notification.js
+++ src/components/Notification/notification.js
@@ -6,11 +6,14 @@ import Tippy from "@tippyjs/react";
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/scale-extreme.css';
 import './notification.scss'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Tooltip} from "@mui/material";
+import axios from "axios";
 
 export function Notification() {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"))
     const [open, setOpen] = useState(false);
+    const [type, setType] = useState('all')
     const handleToggle = () => {
         setOpen(!open)
     }
@@ -18,29 +21,30 @@ export function Notification() {
         {
             id: 1,
             content: "Nhu cầu nhân sự abc xyz vừa bị từ chối",
-            time: "1 phút trước",
-            status: "un_read",
+            timestamp: "1 phút trước",
+            link: "",
+            isRead: false,
 
         }, {
             id: 2,
             content: "Nhu cầu nhân sự tháng 3 vừa cập nhật trạng thái: Bị từ chối bởi DET. ",
-            time: "1 giờ trước",
-            status: "un_read",
+            timestamp: "1 giờ trước",
+            isRead: false,
         }, {
             id: 3,
             content: "Nhu cầu nhân sự tháng 3 vừa cập nhật trạng thái: Đang tuyển dụng",
-            time: "5 giây trước",
-            status: "un_read",
+            timestamp: "5 giây trước",
+            isRead: false,
         }, {
             id: 4,
             content: "Nhu cầu nhân sự abc xyz vừa bị tử chối",
-            time: "3 ngày trước",
-            status: "un_read",
+            timestamp: "3 ngày trước",
+            isRead: true,
         }, {
             id: 5,
             content: "Nhu cầu nhân sự abc xyz vừa bị tử chối",
-            time: "5 ngày trước",
-            status: "readed",
+            timestamp: "5 ngày trước",
+            isRead: true,
         },
     ]);
 
@@ -67,40 +71,64 @@ export function Notification() {
             color: 'white'
         }
     }));
+
     const markReaded = (index) => {
-        datas[index].status = 'readed'
+        datas[index].isRead = true
         setDatas([...datas])
     }
+    useEffect(() => {
+        console.log(datas)
+        axios.get(`http://localhost:8080/api/notifications/${currentUser.id}`, {
+            params: {
+                type: type
+            }
+        }).then(
+            response => {
+                setDatas(response.data);
+            }
+        )
+    }, [type]);
+
+    const sendDatas = async () => {
+        try {
+            const response = await axios.put(`http://localhost:8080/api/notifications/${currentUser.id}`, datas);
+        } catch (error) {
+            console.error('Error sending data:', error);
+        }
+    };
 
     return (
         <Tippy
             placement={"bottom-end"}
-            onClickOutside={handleToggle}
+            onClickOutside={() => {sendDatas(); handleToggle()}}
             visible={open}
             interactive
-            trigger={'manual'}
             render={() => (
                 <div className={"notification-container"}>
                     <div className={"wrapper"}>
                         <div className={"header"}>
                             <h4 className={"title"}>Thông báo</h4>
                             <div className={"btns"}>
-                                <div className={"btn"}>
+                                <div className={"btn"} onClick={() => {sendDatas() ;setType('all')}}>
                                     Tất cả
                                 </div>
-                                <div className={"btn"}>
+                                <div className={"btn"} onClick={() => {sendDatas(); setType('un_read')}}>
                                     Chưa đọc
                                 </div>
                             </div>
                         </div>
                         <div className={"body"}>
                             {datas.map((notiItem, index) => (
-                                <div key={notiItem.id} className={"noti-item"} onClick={() => {markReaded(index)}} >
+                                <div key={notiItem.id} className={"noti-item"} onClick={() => {
+                                    markReaded(index)
+                                }}>
                                     <div className={"content"}>{notiItem.content}</div>
                                     <div className={"time"}>{notiItem.time}</div>
-                                    {notiItem.status === 'un_read' &&
+                                    {!notiItem.isRead &&
                                         <Tooltip title={"Đánh dấu là đã đọc"}>
-                                            <div className={"read-doc"} onClick={() => {markReaded(index)}}></div>
+                                            <div className={"read-doc"} onClick={() => {
+                                                markReaded(index)
+                                            }}></div>
                                         </Tooltip>
                                     }
                                 </div>
