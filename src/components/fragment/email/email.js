fc2cfc31c39f07fe085ed8e11805898a131bd87a
thamuz24 <nguyenhoanggiaminh24@gmail.com>
2024-06-06 16:06:35 +0700

    call Api email

--- src/components/fragment/email/email.js
+++ src/components/fragment/email/email.js
@@ -1,73 +1,84 @@
 import { useEffect, useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
+import axios from 'axios';
 
 export default function Email() {
-    const [dataSendPersonalNeed, setDataSendPersonalNeed] = useState([
-        {
-            toEmail: 'tronglai301@gmail.com',
-            toName: 'Trọng',
-            internNeeds: 4,
-            internNeedHandOver: 1,
-            namePersonalNeeds: 'Nhu cầu nhân sự tháng 6',
-            dateStart: '12/03/2024',
-            dateEnd: '12/04/2024',
-            linkProgress: 13,
-            totalIntern: 20,
-            passIntern: 15,
-            trainningIntern: 10,
-            handOverIntern: 3,
-            failIntern: 3,
-            notTrainningIntern: 3
-        },
-        {
-            toEmail: 'gabynexo221@gmail.com',
-            toName: 'Bảo ngu',
-            internNeeds: 4,
-            internNeedHandOver: 1,
-            namePersonalNeeds: 'Nhu cầu nhân sự tháng 6',
-            dateStart: '12/03/2024',
-            dateEnd: '12/04/2024',
-            totalIntern: 20,
-            passIntern: 15,
-            trainningIntern: 10,
-            handOverIntern: 3,
-            failIntern: 3,
-            notTrainningIntern: 3,
-            linkProgress: 13
-        }
-    ]);
-    const [dataSendRecruitmentPlan, setDataSendRecruitmentPlan] = useState([
-        {
-            toEmail: 'vantuanvuong69@gmail.com',
-            toName: 'Tuấn',
-            internNeeds: 4,
-            internNeedHandOver: 1,
-            nameRecuitmentPlan: 'Kế hoạch tuyển dụng tháng 6',
-            linkProgress: 2,
-            dateStart: '12/03/2024',
-            dateEnd: '12/04/2024',
-            totalIntern: 20,
-            passIntern: 15,
-            trainningIntern: 10,
-            handOverIntern: 3,
-            failIntern: 3,
-            notTrainningIntern: 3
-        }
-    ]);
+    const [toSend, setToSend] = useState([]);
+    const [dataSendPersonalNeed, setDataSendPersonalNeed] = useState([]);
+    const [dataSendRecruitmentPlan, setDataSendRecruitmentPlan] = useState([]);
+
+    const emailApi = () => {
+        axios.get('http://localhost:8080/api/send/').then(res => {
+            setToSend(res.data.emailData);
+        })
+
+    }
+    useEffect(() => {
+        emailApi();
+    }, [])
+
+    const check = () => {
+        const recruitmentPlan = [];
+        const personalNeed = [];
+        toSend.forEach(item => {
+            console.log(item.isRequest);
+            if (item.isRequest === 0) {
+                recruitmentPlan.push(item);
+            }
+            if (item.isRequest === 1) {
+                personalNeed.push(item);
+            }
+        })
+        setDataSendPersonalNeed(personalNeed);
+        setDataSendRecruitmentPlan(recruitmentPlan);
+    }
+
+
+    useEffect(() => {
+        check();
+    }, [toSend])
+
+
     console.log(dataSendRecruitmentPlan)
-    const serviceId = process.env.REACT_APP_API_SERVICE_ID ;
-    const templateIdRecruitmentPLan = process.env.REACT_APP_API_TEMPLATE_RECRUITMENT_PLAN_ID ;
+    console.log(dataSendPersonalNeed)
+    const serviceId = process.env.REACT_APP_API_SERVICE_ID;
+    const templateIdRecruitmentPLan = process.env.REACT_APP_API_TEMPLATE_RECRUITMENT_PLAN_ID;
     const templateIdPersonalNeed = process.env.REACT_APP_API_TEMPLATE_PERSONAL_NEED_ID;
     const publicKey = process.env.REACT_APP_API_PUBLIC_KEY;
     const intervalRef = useRef(null);
- 
+    const sendEmailBoth = (item,template) => {
+        const templateParamsRcruitmentPLan = {
+            to_email: item.toEmail,
+            to_name: item.toName,
+            intern_needs: item.internNeeds,
+            intern_need_hand_over: item.internNeedHandOver,
+            name_recruitment_plan: item.namePersonalNeeds,
+            link_progress: item.linkProgress,
+            date_start: item.dateStart,
+            date_end: item.dateEnd,
+            total_intern: item.totalIntern,
+            pass_intern: item.passIntern,
+            trainning_intern: item.trainningIntern,
+            hand_over_intern: item.handOverIntern,
+            fail_intern: item.failIntern,
+            not_trainning_intern: item.notTrainningIntern
+        }
+        emailjs.send(serviceId, template, templateParamsRcruitmentPLan, publicKey).then(
+            (response) => {
+                console.log('Success!', response);
+            }, (error) => {
+                console.log(error.text);
+            }
+        )
+    }
+
     const sendEmailRecuitmentPlan = (item) => {
         const templateParamsRcruitmentPLan = {
             to_email: item.toEmail,
             to_name: item.toName,
             intern_needs: item.internNeeds,
             intern_need_hand_over: item.internNeedHandOver,
-            name_recruitment_plan: item.nameRecuitmentPlan,
+            name_recruitment_plan: item.namePersonalNeeds,
             link_progress: item.linkProgress,
             date_start: item.dateStart,
             date_end: item.dateEnd,
@@ -113,11 +124,11 @@ export default function Email() {
         )
     }
 
-    const sendEmail = () =>{
+    const sendEmail = () => {
         sendEmailRecuitmentPlan();
         sendEmailPersonalneed();
     }
-  
+
     useEffect(() => {
         const dayNow = new Date();
         const dayCheck = dayNow.getDay();
@@ -136,18 +147,17 @@ export default function Email() {
         // send check
 
         // dataSendPersonalNeed.map(item => {
-        //     sendEmailPersonalneed(item);
+        //     sendEmailBoth(item,templateIdPersonalNeed);
         // })
+
         // dataSendRecruitmentPlan.map(item => {
-        //     sendEmailRecuitmentPlan(item);
+        //     sendEmailBoth(item,templateIdRecruitmentPLan);
         // })
-
-
-
-
+  
         intervalRef.current = setInterval(sendEmail, 24 * 60 * 60 * 1000);
         return () => clearInterval(intervalRef.current);
 
     }, [])
+    
     return null;
 }
