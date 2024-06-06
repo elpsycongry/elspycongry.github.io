f25fe32d2360bbe3debfd7ed6cb54ef250f2c1dd
elspycongry <vuthanhtungcv1@gmail.com>
2024-06-06 16:49:17 +0700

    fasđ

---
+++ src/components/Notification/submenuNotification.js
@@ -0,0 +1,37 @@
+import {Done, MoreHoriz, MoreVert} from "@mui/icons-material";
+import * as React from "react";
+import Tippy from "@tippyjs/react";
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale-extreme.css';
+import './submenunoti.scss'
+import {useState} from "react";
+
+export function SubmenuNotification({markAllReaded}) {
+    const [open, setOpen] = useState(false);
+    const toggle = () => {
+        console.log(open)
+        setOpen(!open)
+    }
+    return (
+        <>
+            <Tippy
+
+                animation={false}
+                onClickOutside={toggle}
+                visible={open}
+                placement={'bottom-end'}
+                interactive
+                render={(...attrs) =>
+                    <div className={'setting-container'}>
+                        <div className={"setting-items"}>
+                            <div className={'setting-item'} onClick={markAllReaded}><Done className={'icon'} />Đánh dấu tất cả là đã đọc</div>
+                        </div>
+                    </div>
+                }
+            >
+                <div className={"setting-icon"}>
+                    <MoreHoriz  onClick={toggle}></MoreHoriz>
+                </div>
+            </Tippy></>
+    )
+}
